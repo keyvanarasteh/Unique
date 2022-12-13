4f5d2413066d1d4b08d7b4e24ae8f7f2ea05323f
AdemMertBiyik <a.mertbyk0053@gmail.com>
2022-12-13 22:07:04 +0300

    ilk commit

---
+++ odev/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:odev/screens/screen1.dart';
+import 'package:odev/screens/screen2.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: 'Flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(), 
+      home: const screen2(),
+    );
+  }
+}
+
